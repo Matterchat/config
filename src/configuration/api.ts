599241a471b1a22225ea47d73e0c058f9215b3af
Martin Petr <martinpetrnp@gmail.com>
2026-07-23 13:22:02 +0200

    Add S3 config

--- src/configuration/api.ts
+++ src/configuration/api.ts
@@ -18,4 +18,12 @@ export class ApiConfiguration {
     apiKey: process.env.LIVEKIT_API_KEY || "",
     apiSecret: process.env.LIVEKIT_API_SECRET || "",
   } as const;
+
+  public static readonly s3 = {
+    endpoint: process.env.S3_ENDPOINT || "",
+    accessKeyId: process.env.S3_ACCESS_KEY || "",
+    secretAccessKey: process.env.S3_SECRET_KEY || "",
+    bucketName: process.env.S3_BUCKET_NAME || "",
+    publicUrl: process.env.S3_PUBLIC_URL || "",
+  } as const;
 }
